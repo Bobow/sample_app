bd49317689b79c675b52f31662a72d9a30c0f42f
Bobow <bjaurena@octo.com>
2010-12-15 15:28:39 +0100

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   
+  match '/signup', :to => 'users#new'
+  
   root :to => 'pages#home'
+
   
   # get "pages/home"
   # get "pages/contact"
